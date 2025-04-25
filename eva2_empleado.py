ea03cf3c937f2a01acc88a9c9d6772c6cc8c4c76
scabezasInacap <sebastian.cabezas07@inacapmail.cl>
2025-04-25 13:37:55 -0400

    Create eva2_empleado.py

---
+++ eva2_empleado.py
@@ -0,0 +1,58 @@
+class Empleado:
+    #constructor con parámetros
+    def __init__(self, id, nombre, direccion, telefono, correo, fecha_inicio, salario, activo):
+        self._id:int = id
+        self._nombre:str = nombre
+        self._direccion:str = direccion
+        self._telefono:str = telefono
+        self._correo:str = correo
+        self._fecha_inicio = fecha_inicio
+        self._salario:int = salario
+        self._activo: bool = activo
+        # pertenece a un departamento
+        self._departamento = None
+        # puede estar en mas de un proyecto
+        self._proyectos = []
+    
+    #encapsulamiento: accesadores (getters) y mutadores (setters)
+    def getId(self): return self._id
+    def setId(self, nuevo): self._id = nuevo
+
+    def getNombre(self): return self._nombre
+    def setNombre(self, nuevo): self._nombre = nuevo
+
+    def getDireccion(self): return self._direccion
+    def setDireccion(self, nuevo): self._direccion = nuevo
+
+    def getTelefono(self): return self._telefono
+    def setTelefono(self, nuevo): self._telefono = nuevo
+
+    def getCorreo(self): return self._correo
+    def setCorreo(self, nuevo): self._correo = nuevo
+
+    def getFechaInicio(self): return self._fecha_inicio
+    def setFechaInicio(self, nuevo): self._fecha_inicio = nuevo
+
+    def getSalario(self): return self._salario
+    def setSalario(self, nuevo): self._salario = nuevo
+
+    def getActivo(self): return self._activo
+    def setActivo(self, nuevo): self._activo = nuevo
+
+    def getDepartamento(self): return self._departamento
+    def setDepartamento(self, nuevo): self._departamento = nuevo
+
+    def getProyectos(self): return self._proyectos
+    def setProyectos(self, nuevo): self._proyectos = nuevo
+
+    #toString para mostrar
+    def __str__(self):
+        return f"Empleado [id: {self._id}]\n[nombre: {self._nombre}]\n[direccion: {self._direccion}]"
+
+#APP
+#empleado1 es una instancia de un objeto Empleado
+empleado1 = Empleado(111, "Sebastian", "Calle 1", 95618, "scabezas@inacap.cl", "08/06/1987", 1234, True)
+# print(empleado1) #va por el toString (__str__)
+# print(empleado1.getId())
+# empleado1.setId(123)
+# print(empleado1.getId())
