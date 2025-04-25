e88e320b2816677238352d0910e3671e7b2717e2
scabezasInacap <sebastian.cabezas07@inacapmail.cl>
2025-04-25 13:41:00 -0400

    Create eva2_departamento.py

---
+++ eva2_departamento.py
@@ -0,0 +1,28 @@
+class Departamento: 
+    def __init__(self, id, nombre, gerente, activo): 
+        self._id = id 
+        self._nombre = nombre 
+        self._gerente = gerente 
+        self._activo = activo
+
+    #encapsulamiento: accesadores (getters) y mutadores (setters)
+    def getId(self): return self._id
+    def setId(self, nuevo): self._id = nuevo
+
+    def getNombre(self): return self._nombre
+    def setNombre(self, nuevo): self._nombre = nuevo
+
+    def getGerente(self): return self._nombre
+    def setGerente(self, nuevo): self._gerente = nuevo
+
+    def getActivo(self): return self._activo
+    def setActivo(self, nuevo): self._activo = nuevo
+
+    #toString para mostrar
+    def __str__(self):
+        return f"Departamento [id: {self._id}]\n[nombre: {self._nombre}]\n[gerente: {self._gerente}]"
+    
+
+#Implementacion APP
+depa1 = Departamento(1, "TI", "Seba Cabezas", True)
+print(depa1)
